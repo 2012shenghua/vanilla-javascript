c4caa0e9eb4235b0db9ac29139c6263ccc0fc405
jasminecjc <249485410@qq.com>
2016-10-02 00:29:32 +0800

    update

--- 对象扁平化.js
+++ 对象扁平化.js
@@ -15,13 +15,9 @@ var obj = {
    } 
 };//f,g,i,c,e,h,k,b,d,j,a
 
-const arrayIncludes = (arr, a) => arr.toString().includes(a.toString()) ;
-
 const sear = (obj, deep, arr) => {
+    !arr[deep] ? (arr[deep] = Object.keys(obj) ) : (arr[deep] = arr[deep].concat(Object.keys(obj)));
     for(let key in obj) {
-        !arr[deep] && (arr[deep] = []);
-        var keys = Object.keys(obj);
-        !arrayIncludes(arr[deep], keys) && arr[deep].push(Object.keys(obj));
         if(typeof obj[key] === 'object') {
             sear(obj[key], deep+1,arr);
         }
@@ -29,7 +25,7 @@ const sear = (obj, deep, arr) => {
     return arr;
 }
 
-console.log(sear(obj, 0, {}))
-var res = _.flattenDeep(sear(obj, 0, []).reverse());
+console.log(sear(obj, 0, {}));
+var res = sear(obj, 0, []).reverse().join(',');
 console.log(res);
 
